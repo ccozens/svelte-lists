88bc602bf44a7dddfde3563b9f1f34b9aceb6a79
ccozens <ccozens@gmail.com>
2024-06-01 11:40:52 +0100

    started tutorial and copied and pasted css, with some updates as tutorial uses open props

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,15 @@
 </script>
 
 {@render children()}
+
+<style>
+	:global(html, body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		display: grid;
+		place-content: center;
+		background-color: hsl(220 10% 8%);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,51 @@
 <script>
-    import { Alert } from 'flowbite-svelte';
-  </script>
+	let todos = $state([
+		{ text: 'Todo 1', done: false },
+		{ text: 'Todo 2', done: true },
+		{ text: 'Todo 3', done: false }
+	]);
+</script>
 
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
+<div class="todos">
+	{#each todos as todo, i}
+		<div class="todo">
+			<input value={todo.text} type="text" />
+			<input value={todo.done} type="checkbox" />
+		</div>
+	{/each}
+</div>
+
+<style>
+	.todos {
+		display: grid;
+		gap: 1rem;
+		margin-block-start: 1rem;
+	}
+
+	.todo {
+        position: relative;
+		transition: opacity 0.3s;
+	}
+
+	.completed {
+		opacity: 0.4;
+	}
+
+	input[type='text'] {
+        background-color: dimgrey;
+        color: white;
+		width: 100%;
+		padding: 1rem;
+	}
+
+	input[type='checkbox'] {
+		position: absolute;
+		right: 4%;
+		top: 50%;
+		translate: 0% -50%;
+	}
+
+	.filters {
+		margin-block: 1rem;
+	}
+</style>
