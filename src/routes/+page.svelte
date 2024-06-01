366e415918c3eabc9cd4321063742b31629b4f4e
ccozens <ccozens@gmail.com>
2024-06-01 13:32:02 +0100

    corrected let filters name, added function to toggle filter and buttons for filtering and filter function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,78 @@
 <script lang="ts">
-type Todo = {
-    text: string;
-    done: boolean;
-};
-type Filters = 'all' | 'active' | 'completed';
+	type Todo = {
+		text: string;
+		done: boolean;
+	};
+	type Filters = 'All' | 'Active' | 'Completed';
 
 	let todos = $state<Todo[]>([]);
-    let filters = $state<Filters>('all');
+	let filter = $state<Filters>('All');
+	let filteredTodos = $derived(filterTodos(filter, todos));
 
-    function addTodo(event: KeyboardEvent) {
-        // check if enter key was pressed
-        if (event.key !== 'Enter') return;
+	function addTodo(event: KeyboardEvent) {
+		// check if enter key was pressed
+		if (event.key !== 'Enter') return;
 
-        // type todo
-        const todoElement = event.target as HTMLInputElement;
-        const text = todoElement.value;
-        // add to todos array
-        todos = [...todos, { text, done: false }];
+		// type todo
+		const todoElement = event.target as HTMLInputElement;
+		const text = todoElement.value;
+		// add to todos array
+		todos = [...todos, { text, done: false }];
 
-        // blank input after adding to array
-        todoElement.value = '';
-    }
+		// blank input after adding to array
+		todoElement.value = '';
+	}
 
-    function editTodo(event: Event) {
-        // extract and type todo
-        const inputEl = event.target as HTMLInputElement;
-        // get index
-        // works as set data-index={i} on input in HTML markup
-        // note '+' casts it as a number and ! silences ts error
-        const index = +inputEl.dataset.index!
+	function editTodo(event: Event) {
+		// extract and type todo
+		const inputEl = event.target as HTMLInputElement;
+		// get index
+		// works as set data-index={i} on input in HTML markup
+		// note '+' casts it as a number and ! silences ts error
+		const index = +inputEl.dataset.index!;
 
-        // reassign todo text in array
-        todos[index].text = inputEl.value;
-    }
+		// reassign todo text in array
+		todos[index].text = inputEl.value;
+	}
 
-    function toggleTodo(event: Event) {
-        const inputEl = event.target as HTMLInputElement;
-        const index = +inputEl.dataset.index!
-        todos[index].done = !todos[index].done;
-    };
+	function toggleTodo(event: Event) {
+		const inputEl = event.target as HTMLInputElement;
+		const index = +inputEl.dataset.index!;
+		todos[index].done = !todos[index].done;
+	}
 
+	function setFilter(newFilter: Filters) {
+		filter = newFilter;
+	}
+
+	function filterTodos(filter: Filters, todos: Todo[]) {
+		switch (filter) {
+			case 'All':
+				return todos;
+			case 'Active':
+				return todos.filter((todo) => !todo.done);
+			case 'Completed':
+				return todos.filter((todo) => todo.done);
+			default:
+				return todos;
+		}
+	}
 </script>
 
-<input onkeydown={addTodo} placeholder="Add todo" type="text"  />
+<input onkeydown={addTodo} placeholder="Add todo" type="text" />
 
 <div class="todos">
-	{#each todos as todo, i}
+	{#each filteredTodos as todo, i}
 		<div class="todo">
 			<input oninput={editTodo} data-index={i} value={todo.text} type="text" />
 			<input onchange={toggleTodo} data-index={i} value={todo.done} type="checkbox" />
 		</div>
+	{/each}
+</div>
+
+<div class="filters">
+	{#each ['All', 'Active', 'Completed'] as filter}
+		<button onclick={() => setFilter(filter)}>{filter}</button>
 	{/each}
 </div>
 
@@ -61,17 +84,17 @@ type Filters = 'all' | 'active' | 'completed';
 	}
 
 	.todo {
-        position: relative;
+		position: relative;
 		transition: opacity 0.3s;
 	}
 
-	.completed {
+	.Completed {
 		opacity: 0.4;
 	}
 
 	input[type='text'] {
-        background: #263238;
-        color: white;
+		background: #263238;
+		color: white;
 		width: 100%;
 		padding: 1rem;
 	}
@@ -85,5 +108,16 @@ type Filters = 'all' | 'active' | 'completed';
 
 	.filters {
 		margin-block: 1rem;
+	}
+
+	button {
+		margin-inline-end: 1rem;
+		background: #727475;
+		color: white;
+		border: none;
+		padding: 0.5rem 1rem;
+		border-radius: 0.25rem;
+		cursor: pointer;
+		transition: All 0.3s;
 	}
 </style>
