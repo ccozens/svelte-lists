b025749abb2f671e0d9bb16a0a7cc4f38da9280e
ccozens <ccozens@gmail.com>
2024-06-01 10:50:07 +0100

    added flowbite-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<h1 class="text-3xl font-bold underline text-red-900">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { Alert } from 'flowbite-svelte';
+  </script>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+  <div class="p-8">
+    <Alert>
+      <span class="font-medium">Info alert!</span>
+      Change a few things up and try submitting again.
+    </Alert>
+  </div>
