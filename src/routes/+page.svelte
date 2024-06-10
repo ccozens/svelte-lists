8551654e122a53c9d5c0dcab1ccb3a674be31427
ccozens <ccozens@gmail.com>
2024-06-10 13:46:43 +0100

    restyled homepage and added inter font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,6 @@
 		}
 	}
 
-
 	let buttonStyle = 'bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 p-3 rounded-lg';
 	let buttonActive =
 		'transition duration-600 active:ring-2 active:ring-offset-2 active:ring-pink-700';
@@ -55,16 +54,16 @@
 
 <div class="grid gap-4 m-4">
 	{#each filteredTodos as todo, todoIndex}
-		<div class="">
-			<h2 class="text-3xl text-center my-3">{todo.heading}</h2>
+		<div class="flex flex-col items-center bg-slate-900 border-2 border-slate-600">
+			<h2 class="text-3xl text-center my-3 ">{todo.heading}</h2>
 			{#each todo.tasks as task, taskIndex}
 				<button
 					onclick={toggleTodo}
 					data-todoIndex={todoIndex}
 					data-taskIndex={taskIndex}
 					class={task.done
-						? 'bg-indigo-950 w-full p-2 m-2 rounded-sm transition duration-300 opacity-10 '
-						: 'bg-indigo-950 w-full p-2 m-2 rounded-sm '}
+						? 'bg-slate-800 p-2 mb-1 rounded-sm transition duration-300 opacity-10 '
+						: 'bg-slate-700 opacity-90 w-11/12 p-2 mb-1 rounded-sm'}
 					>{task.text}
 				</button>
 			{/each}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	plugins: [],
 
-
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: [
+					'Inter Variable',
+					'ui-sans-serif',
+					'system-ui',
+					'sans-serif',
+					'"Apple Color Emoji"',
+					'"Segoe UI Emoji"',
+					'"Segoe UI Symbol"',
+					'"Noto Color Emoji"'
+				]
+			}
 		}
 	}
 };
