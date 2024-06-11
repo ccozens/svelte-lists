20bb1303102100ece6b3d6dbfe8b8b2408abef8d
ccozens <ccozens@gmail.com>
2024-06-11 12:48:54 +0100

    added to todo list

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,53 +1,87 @@
-import type { Todo } from "./types";
+import type { Todo } from './types';
 
 export const groupedTodos: Todo[] = [
-    {
-        heading: "Bradley's room",
-        tasks: [
-            { text: "Music and camera on", done: false },
-            { text: "Nappy bucket and wipes", done: false },
-            { text: "Change mat on the floor with green towel on top, then nappy with three liners", done: false },
-            { text: "Pyjamas and socks appropriate to the weather", done: false },
-            { text: "Dressing gown in spare towel on the side", done: false },
-            { text: "Potty and kneeler in place", done: false },
-            { text: "Black sponge bag, inhaler if needed both on top", done: false },
-            { text: "Three books on corner of white unit", done: false },
-            { text: "Duvet pulled back", done: false },
-            { text: "Bradley's clothes out for the next day and in our room", done: false },
-        ],
-    },
-    {
-        heading: "Bathroom",
-        tasks: [
-            { text: "Bath run to right temp", done: false },
-            { text: "Cloths in the bath", done: false },
-            { text: "Soapy cloth on the side", done: false },
-            { text: "Two floor towels on floor by bath", done: false },
-            { text: "Bradley and Emily towels on side", done: false },
-            { text: "Shampoo, conditioner, hair brush as appropriate", done: false },
-            { text: "Both water bottles in bathroom", done: false },
-            { text: "Mouthwash", done: false },
-            { text: "Bradley and Emily's toothbrushes with Emily's out of the bathroom, both with toothpaste", done: false },
-        ],
-    },
-    {
-        heading: "Emily's room",
-        tasks: [
-            { text: "Pull duvet to side", done: false },
-            { text: "Curtains closed", done: false },
-            { text: "Music on", done: false },
-            { text: "Neck warmer/gloves, light, knee cushion out of drawers", done: false },
-            { text: "Red light on", done: false },
-        ],
-    },
-    {
-        heading: "Other",
-        tasks: [
-            { text: "Bradley’s 6 book choice", done: false },
-            { text: "Change blues (vapouriser refills) if needed", done: false },
-            { text: "Close curtains upstairs", done: false },
-            { text: "Emily’s clothes out and in pink bag", done: false },
-            { text: "Sleep diary", done: false },
-        ],
-    },
+	{
+		heading: "Bradley's room",
+		tasks: [
+			{ text: 'Music and camera on', done: false },
+			{ text: 'Nappy bucket and wipes', done: false },
+			{
+				text: 'Change mat on the floor with green towel on top, then nappy with three liners',
+				done: false
+			},
+			{ text: 'Pyjamas and socks appropriate to the weather', done: false },
+			{ text: 'Dressing gown in spare towel on the side', done: false },
+			{ text: 'Potty, nappy bucket and kneeler in place', done: false },
+			{ text: 'Black sponge bag, inhaler if needed both on top', done: false },
+			{ text: 'Three books on corner of white unit', done: false },
+			{ text: 'Duvet pulled back', done: false },
+			{ text: "Bradley's clothes out for the next day and in our room", done: false }
+		]
+	},
+	{
+		heading: 'Bathroom',
+		tasks: [
+			{ text: 'Bath run to right temp', done: false },
+			{ text: 'Cloths in the bath', done: false },
+			{ text: 'Soapy cloth on the side', done: false },
+			{ text: 'Two floor towels on floor by bath', done: false },
+			{ text: 'Bradley and Emily towels on side', done: false },
+			{ text: 'Shampoo, conditioner, hair brush as appropriate', done: false },
+			{ text: 'Both water bottles in bathroom', done: false },
+			{ text: 'Mouthwash', done: false },
+			{
+				text: "Bradley and Emily's toothbrushes with Emily's out of the bathroom, both with toothpaste",
+				done: false
+			}
+		]
+	},
+	{
+		heading: "Emily's room",
+		tasks: [
+			{ text: 'Pull duvet to side', done: false },
+			{ text: 'Curtains closed', done: false },
+			{ text: 'Music on', done: false },
+			{ text: 'Neck warmer/gloves, light, knee cushion out of drawers', done: false },
+			{ text: 'Red light on', done: false }
+		]
+	},
+	{
+		heading: 'Other',
+		tasks: [
+			{ text: 'Suzi exercises', done: false },
+			{ text: "Bradley's 6 book choice", done: false },
+			{ text: 'Change blues (vapouriser refills) if needed', done: false },
+			{ text: 'Close curtains upstairs', done: false },
+			{ text: "Emily's clothes out and in pink bag", done: false },
+			{ text: 'Sleep diary', done: false },
+			{ text: 'Washing away', done: false },
+			{ text: 'Swap nappy buckets', done: false }
+		]
+	},
+
+	{
+		heading: 'After bath',
+		tasks: [
+			{ text: 'Toilet box', done: false },
+			{ text: 'Suzi clothes', done: false },
+			{ text: 'Write in book', done: false },
+			{ text: 'Photos', done: false }
+		]
+	},
+	{
+		heading: 'Downstairs',
+		tasks: [
+			{ text: 'Buggy', done: false },
+			{ text: 'Dishwasher', done: false },
+			{ text: 'Food planning', done: false },
+			{ text: 'Change bag', done: false },
+			{ text: 'Nappies in grey basket', done: false },
+			{ text: 'Coconut water and tomatoes upstairs', done: false },
+			{ text: 'Washing on', done: false },
+			{ text: 'Bread?', done: false },
+			{ text: 'Snacks out', done: false },
+			{ text: 'Fruit / wipes in smiggle', done: false }
+		]
+	}
 ];
