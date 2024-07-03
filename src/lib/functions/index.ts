f49ca33e8d37b7c402e567338b12eb5138db8b86
ccozens <ccozens@gmail.com>
2024-07-03 11:36:34 +0100

    removed functions folder as not using

--- src/lib/functions/index.ts
+++
@@ -1,6 +0,0 @@
-// export * from "./addTodo";
-// export * from "./deleteTodo";
-// export * from "./toggleTodo";
-// export * from "./editTodo";
-// export * from "./checkTodo";
-// export * from "./getTodos";
--- src/lib/functions/toggleTodo.ts
+++
@@ -1,6 +0,0 @@
-// function to toggle todo
-export function toggleTodo(event: Event) {
-	const inputEl = event.target as HTMLInputElement;
-	const index = +inputEl.dataset.index!;
-	todos[index].done = !todos[index].done;
-}
